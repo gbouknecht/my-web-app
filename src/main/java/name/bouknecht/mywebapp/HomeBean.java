2a5b7e56db4c2f61088e69dea73c5ea558655f21
Gerrit N. Bouknecht <gbouknecht@server.fake>
2011-10-25 14:10:14 +0200

    Add HomeBean and logging

---
+++ src/main/java/name/bouknecht/mywebapp/HomeBean.java
@@ -0,0 +1,19 @@
+package name.bouknecht.mywebapp;
+
+import javax.faces.bean.ManagedBean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ManagedBean
+public class HomeBean {
+    private static final Logger logger = LoggerFactory.getLogger(HomeBean.class);
+
+    public HomeBean() {
+        logger.info("Constructing " + HomeBean.class);
+    }
+
+    public String getMessage() {
+        return "Hello, from " + HomeBean.class;
+    }
+}
