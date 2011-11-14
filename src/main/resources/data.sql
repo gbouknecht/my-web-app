1196f3471d6694b044f946ff82869308f37bee59
Gerrit N. Bouknecht <gbouknecht@server.fake>
2011-11-14 10:02:06 +0100

    Let Hibernate create database schema
    Instead of using schema.sql, Hibernate will create the schema at
    application startup. The data.sql will still be used to fill the
    database with data after creation of the schema.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into account (id, user_id, firstname, lastname) values (next value for id_seq, 'ppieterse', 'Piet', 'Pieterse');
-insert into account (id, user_id, firstname, lastname) values (next value for id_seq, 'jjansse', 'Jan', 'Jansse');
-insert into account (id, user_id, firstname, lastname) values (next value for id_seq, 'ggerritse', 'Gerrit', 'Gerritse');
+insert into account (id, user_id, firstname, lastname) values (next value for account_id_seq, 'ppieterse', 'Piet', 'Pieterse');
+insert into account (id, user_id, firstname, lastname) values (next value for account_id_seq, 'jjansse', 'Jan', 'Jansse');
+insert into account (id, user_id, firstname, lastname) values (next value for account_id_seq, 'ggerritse', 'Gerrit', 'Gerritse');
